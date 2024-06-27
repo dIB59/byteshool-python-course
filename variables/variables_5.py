dd618191fe7929421747ae793173d635a5a1f4de
LegolasArkantos <unasaamir101@gmail.com>
2024-06-27 16:38:27 +0500

    varaibles 5

---
+++ variables/variables_5.py
@@ -0,0 +1,14 @@
+#write code to swap the values of x and y , so x becomes 10 and y becomes 5 when you print them
+#hint: u may need to use a temporary variable to help you out
+
+x = 5  #do not change this line
+y = 10  #do not change this line
+
+#write code here
+
+
+
+
+print("Value of x after swap:", x)  #do not change this line
+print("Value of y after swap:", y)  #do not change this line
+
