0efc02c9db3e38bb2311b7dacae759f7df4a7954
LegolasArkantos <unasaamir101@gmail.com>
2024-06-27 20:00:35 +0500

    primative 1

---
+++ primative_types/primative_1.py
@@ -0,0 +1,12 @@
+# Exercise 1: Booleans
+
+
+#example
+is_morning = True
+if is_morning:
+    print("Good morning!")
+
+# Finish the rest of this line like the example! Or make it be False!
+is_evening = 
+if is_evening:
+    print("Good evening!")
