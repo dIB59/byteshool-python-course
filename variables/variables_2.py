59635ee2942042f0d62a097acde5246b94c28961
LegolasArkantos <unasaamir101@gmail.com>
2024-06-27 16:27:35 +0500

    varaible 3

--- variables/variables_2.py
+++ variables/variables_2.py
@@ -1,9 +1,9 @@
 #merge both variables to get full name
 #hint: its as simple as adding them together
 
-first_name = "John"
-last_name = " Doe"
+first_name = "John" #do not change this line
+last_name = " Doe" #do not change this line
 
 full_name = 
 
-print("Full Name:", full_name)
+print("Full Name:", full_name) #do not change this line
---
+++ variables/variables_3.py
@@ -0,0 +1,17 @@
+#sum is done as example
+#try to do the rest three using a and b
+
+
+a = 10 #do not change this line
+b = 5 #do not change this line
+
+sum = a + b  #do not change this line
+subtract = 
+Multiply = 
+Divide = 
+
+#do not change lines below
+print("Sum:", sum) 
+print("subtract:", subtract)
+print("Multiply:", Multiply)
+print("Divide:", Divide)
