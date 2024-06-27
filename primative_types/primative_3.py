e420d6d96bbb13433ec372e2096c0326e377f490
LegolasArkantos <unasaamir101@gmail.com>
2024-06-27 20:10:54 +0500

    primative 3

---
+++ primative_types/primative_3.py
@@ -0,0 +1,8 @@
+# Exercise 3: Lists
+
+a =            # make a list, you can make it longer or shorter 
+
+if len(a) >= 10:
+    print("Wow, that's a big list!")
+else:
+    print("Meh, I eat lists like that for breakfast.")
