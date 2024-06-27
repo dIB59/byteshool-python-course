4436336f155e8861e7f49e282d1f4f1483f622dc
LegolasArkantos <unasaamir101@gmail.com>
2024-06-27 20:08:01 +0500

    primative 2

---
+++ primative_types/primative_2.py
@@ -0,0 +1,22 @@
+# Exercise 2: Characters
+
+# example
+
+my_first_initial = 'C'
+if my_first_initial.isalpha():
+    print("Alphabetical!")
+elif my_first_initial.isdigit():
+    print("Numerical!")
+else:
+    print("Neither alphabetic nor numeric!")
+
+
+# Try a letter, try a number, try a special character, try a character,try an emoji! 😊
+ 
+your_character = ''  
+if your_character.isalpha():
+    print("Alphabetical!")
+elif your_character.isdigit():
+    print("Numerical!")
+else:
+    print("Neither alphabetic nor numeric!")
