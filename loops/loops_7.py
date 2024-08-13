d0337c7051f3e348f19bd0441594c83a704786b2
LegolasArkantos <unasaamir101@gmail.com>
2024-08-13 19:15:05 +0500

    corection

--- loops/loops_7.py
+++ loops/loops_7.py
@@ -1,9 +1,10 @@
-# Exercise 10
+# Exercise 7
 
 # Question: Start with an empty string, and use abloop to add each letter from the end of the word to the start.
 word = "hello"
 reversed_word = ""  # Start with an empty string.
 
+#   write your code here
 
 
 print(reversed_word)  # This will print the word "olleh", if you did it right that is.
