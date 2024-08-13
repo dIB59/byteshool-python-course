e03d7a8562d372acd33ba83c0894e4876e304e4f
LegolasArkantos <unasaamir101@gmail.com>
2024-08-13 22:33:23 +0500

    changes

---
+++ lists/Lists_7.py
@@ -0,0 +1,8 @@
+# Exercise 7
+
+# Hint: Use the sort() method to sort the items in a list.
+fruits = ["banana", "apple", "cherry"]
+
+# Write your code here
+
+print(fruits)  # This will print the sorted list.
